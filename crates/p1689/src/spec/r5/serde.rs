c4df3052cf25b051579f055cced236da69b16c3c
silvanshade <silvanshade@users.noreply.github.com>
2024-06-06 13:30:09 -0600

    Add test to ensure only escaped strings are copied

--- crates/p1689/src/spec/r5/serde.rs
+++ crates/p1689/src/spec/r5/serde.rs
@@ -76,9 +76,13 @@ impl<'a> SerializeAs<DepInfo<'a>> for DepInfoUniqueOutputs {
 #[cfg(feature = "deserialize")]
 mod test {
     use proptest::proptest;
+    use rand::prelude::*;
 
     mod r5 {
-        pub use crate::spec::r5::{proptest::strategy, *};
+        pub use crate::{
+            r5::parsers,
+            spec::r5::{proptest::strategy, *},
+        };
     }
 
     #[cfg(feature = "deserialize")]
@@ -92,6 +96,30 @@ mod test {
                 #[test]
                 fn dep_file(input in r5::strategy::dep_file()) {
                     serde_json::from_str::<r5::DepFile>(&input).unwrap();
+                }
+            }
+
+            #[test]
+            fn only_escaped_strings_are_copied() {
+                let rng = &mut rand::thread_rng();
+                let mut bytes = alloc::vec![0u8; 8192];
+                rng.fill_bytes(&mut bytes);
+                let mut u = arbitrary::Unstructured::new(&bytes);
+                let node_count = u.int_in_range(0u8 ..= 16u8).unwrap();
+                let mut dep_file_texts = r5::datagen::graph::GraphGenerator::gen_dep_files(rng, node_count)
+                    .flat_map(|result| result.and_then(r5::datagen::json::pretty_print_unindented));
+                let mut num_files_with_escaped_strings = 0;
+                // NOTE: Keep iterating until at least 16 files with escapes have been checked
+                while num_files_with_escaped_strings < 16 {
+                    if let Some(dep_file_text) = dep_file_texts.next() {
+                        let num_escaped_strings_within_file = crate::util::count_escaped_strings(&dep_file_text).1;
+                        let input = winnow::BStr::new(dep_file_text.as_bytes());
+                        let state = r5::parsers::State::default();
+                        let mut state_stream = winnow::Stateful { input, state };
+                        let dep_file = r5::parsers::dep_file(&mut state_stream).unwrap();
+                        assert_eq!(num_escaped_strings_within_file, dep_file.count_copies());
+                        num_files_with_escaped_strings += u64::from(0 < num_escaped_strings_within_file);
+                    }
                 }
             }
         }
--- crates/p1689/src/spec/r5/winnow.rs
+++ crates/p1689/src/spec/r5/winnow.rs
@@ -461,13 +461,14 @@ pub mod required_module_desc {
 #[cfg(test)]
 mod test {
     use proptest::proptest;
+    use rand::prelude::*;
     use winnow::BStr;
 
     use super::*;
     use crate::util::winnow::State;
 
     mod r5 {
-        pub use crate::spec::r5::{proptest::strategy, winnow};
+        pub use crate::spec::r5::{proptest::strategy, *};
     }
 
     mod parse {
@@ -500,6 +501,27 @@ mod test {
                     let state = State::default();
                     let mut stream = StateStream { input, state };
                     r5::winnow::dep_file::version.parse_next(&mut stream).unwrap();
+                }
+            }
+
+            #[test]
+            fn only_escaped_strings_are_copied() {
+                let rng = &mut rand::thread_rng();
+                let mut bytes = alloc::vec![0u8; 8192];
+                rng.fill_bytes(&mut bytes);
+                let mut u = arbitrary::Unstructured::new(&bytes);
+                let node_count = u.int_in_range(0u8 ..= 16u8).unwrap();
+                let mut dep_file_texts = r5::datagen::graph::GraphGenerator::gen_dep_files(rng, node_count)
+                    .flat_map(|result| result.and_then(r5::datagen::json::pretty_print_unindented));
+                let mut num_files_with_escaped_strings = 0;
+                // NOTE: Keep iterating until at least 16 files with escapes have been checked
+                while num_files_with_escaped_strings < 16 {
+                    if let Some(dep_file_text) = dep_file_texts.next() {
+                        let num_escaped_strings_within_file = crate::util::count_escaped_strings(&dep_file_text).1;
+                        let dep_file = serde_json::from_str::<r5::DepFile>(&dep_file_text).unwrap();
+                        assert_eq!(num_escaped_strings_within_file, dep_file.count_copies());
+                        num_files_with_escaped_strings += u64::from(0 < num_escaped_strings_within_file);
+                    }
                 }
             }
         }
